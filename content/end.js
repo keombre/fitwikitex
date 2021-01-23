7c4e2ed70c73c4b828698736bf8cd72a62aa3558
keombre <keombre8@gmail.com>
2021-01-23 14:59:19 +0100

    initial

---
+++ content/end.js
@@ -0,0 +1,22 @@
+const realSource = "https://fit-wiki.cz/_media/latex/"
+
+const loaded = img => {
+    if (img.complete && img.naturalHeight != 0)
+        return
+    const query = new URLSearchParams(img.src.split("?")[1])
+    if (query.has("media")) {
+        const media = query.get("media")
+        if (media.startsWith("latex:"))
+            img.src = realSource + media.substr(6)
+    }
+}
+
+// attach event listeners
+const images = document.querySelectorAll('img.media[title="mimeTeX"]');
+images.forEach(img => {
+    if (!img.complete) {
+        img.addEventListener('load', () => loaded(img))
+        img.addEventListener('error', () => loaded(img))
+    } else
+        loaded(img)
+})
